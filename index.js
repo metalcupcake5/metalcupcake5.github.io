69e307d008b255449bb2045c05cd835b88afb37f
metalcupcake5 <imcoolcom5@gmail.com>
2022-05-01 10:46:47 -0700

    refactor: allow clicking tippy for pfp source

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 tippy("#pfp", {
-    content: "sauce: https://www.pixiv.net/en/artworks/96755240"
-})
+    content: 'sauce: <a href="https://www.pixiv.net/en/artworks/96755240">pixiv/96755240</a>',
+    interactive: true,
+    allowHTML: true,
+});
 
 tippy("#pronouns", {
     content: "If you know me well I am also fine with she/her",
 });
 
 tippy("#discord", {
-    content: "yes it was a natural discriminator"
-})
+    content: "yes it was a natural discriminator",
+});
