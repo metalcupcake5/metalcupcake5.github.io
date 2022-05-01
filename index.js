c37fa33619964d30abb45effa4241e7eb7d43f0b
metalcupcake5 <imcoolcom5@gmail.com>
2022-05-01 12:21:08 -0700

    feat: add random background selection

--- index.js
+++ index.js
@@ -11,3 +11,29 @@ tippy("#pronouns", {
 tippy("#discord", {
     content: "yes it was a natural discriminator",
 });
+
+const backgrounds = [
+    {
+        path: "/img/backgrounds/1.png",
+        pixiv_id: "94924761",
+    },
+    {
+        path: "/img/backgrounds/2.jpg",
+        pixiv_id: "96981854",
+    },
+    {
+        path: "/img/backgrounds/3.jpg",
+        pixiv_id: "97361753",
+    },
+    {
+        path: "/img/backgrounds/4.png",
+        pixiv_id: "93615405",
+    },
+    {
+        path: "/img/backgrounds/5.png",
+        pixiv_id: "89618492",
+    }
+];
+const bg = backgrounds[Math.floor(Math.random() * backgrounds.length)]
+document.body.style.backgroundImage = `url(${bg.path})`;
+document.querySelector("#bg-source").innerHTML = `Background source: <a href="https://www.pixiv.net/en/artworks/${bg.pixiv_id}">pixiv/${bg.pixiv_id}</a>`;
