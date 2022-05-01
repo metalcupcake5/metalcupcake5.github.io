4dd043ab06a76ca9ce5a304d4f71798bd28b7940
metalcupcake5 <imcoolcom5@gmail.com>
2022-04-30 23:52:18 -0700

    yeah !

---
+++ index.js
@@ -0,0 +1,11 @@
+tippy("#pfp", {
+    content: "sauce: https://www.pixiv.net/en/artworks/96755240"
+})
+
+tippy("#pronouns", {
+    content: "If you know me well I am also fine with she/her",
+});
+
+tippy("#discord", {
+    content: "yes it was a natural discriminator"
+})
