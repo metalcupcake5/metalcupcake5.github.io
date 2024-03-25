8ac2b99a13e5dd013c3768d750ad7ac3977d9cbc
metalcupcake5 <imcoolcom5@gmail.com>
2024-03-25 02:44:16 -0700

    up date

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-tippy("#pfp", {
-    content: 'sauce: <a href="https://www.pixiv.net/en/artworks/96755240">pixiv/96755240</a>',
-    interactive: true,
-    allowHTML: true,
-});
-
-tippy("#pronouns", {
-    content: "If you know me well I am also fine with she/her",
-});
-
-tippy("#discord", {
-    content: "yes it was a natural discriminator",
-});
-
 const backgrounds = [
     {
         path: "/img/backgrounds/1.png",
@@ -35,9 +21,91 @@ const backgrounds = [
     },
     {
         path: "/img/backgrounds/6.jpg",
-        pixiv_id: "98032318"
-    }
+        pixiv_id: "98032318",
+    },
 ];
-const bg = backgrounds[Math.floor(Math.random() * backgrounds.length)]
+const bg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
 document.body.style.backgroundImage = `url(${bg.path})`;
-document.querySelector("#bg-source").innerHTML = `Background source: <a href="https://www.pixiv.net/en/artworks/${bg.pixiv_id}">pixiv/${bg.pixiv_id}</a>`;
+document.querySelector(
+    "#bg-source"
+).innerHTML = `Background source: <a href="https://www.pixiv.net/en/artworks/${bg.pixiv_id}">pixiv/${bg.pixiv_id}</a>`;
+
+const typewriter = document.querySelector("#typewriter");
+const descriptors = [
+    {
+        value: "catgirl",
+        weight: 15,
+    },
+    {
+        value: "transfemme",
+        weight: 10,
+    },
+    {
+        value: "cat",
+        weight: 5,
+    },
+    {
+        value: "idiot",
+        weight: 5,
+    },
+    {
+        value: "computer addict",
+        weight: 2,
+    },
+    {
+        value: "rhythm gamer",
+        weight: 2,
+    },
+];
+let currentDescriptor = descriptors[0].value;
+
+function weightedRandom(weightArr) {
+    let weights = [weightArr[0].weight];
+
+    for (let i = 1; i < weightArr.length; i++)
+        weights[i] = weightArr[i].weight + weights[i - 1];
+
+    let random = Math.random() * weights[weights.length - 1];
+
+    for (let i = 0; i < weights.length; i++)
+        if (weights[i] > random) return weightArr[i].value;
+
+    return weightArr[0].value;
+}
+
+function typeDescriptor() {
+    let current = "";
+    const interval = setInterval(() => {
+        if (current.length == currentDescriptor.length) {
+            clearInterval(interval);
+            setTimeout(() => {
+                deleteDescriptor();
+            }, 2000);
+        }
+        current += currentDescriptor.substring(
+            current.length,
+            current.length + 1
+        );
+        typewriter.innerHTML = current;
+    }, 100);
+}
+
+function deleteDescriptor() {
+    const interval = setInterval(() => {
+        if (typewriter.innerHTML.length == 0) {
+            clearInterval(interval);
+            currentDescriptor = weightedRandom(
+                descriptors.filter((d) => d.value != currentDescriptor)
+            );
+            setTimeout(() => {
+                typeDescriptor();
+            }, 250);
+        }
+        typewriter.innerHTML = typewriter.innerHTML.substring(
+            0,
+            typewriter.innerHTML.length - 1
+        );
+    }, 100);
+}
+
+typeDescriptor();
